493bd446d46a3baa730d1972d1bffdfbfca8d2c5
RISHIT2070 <95566422+RISHIT2070@users.noreply.github.com>
2024-07-26 15:28:40 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+variable "region" {
+  type = string
+}
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+locals {
+  instance_types = {
+    dev  = "t2.micro"
+    prod = "t3.medium"
+  }
+  instance_type = lookup(local.instance_types, var.environment, "t2.micro")
+}
+
+resource "aws_instance" "Infrasity" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = local.instance_type
+  tags = {
+    Name        = "Instance-1"
+    Environment = var.environment
+  }
+}
